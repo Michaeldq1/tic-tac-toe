4826c0c17f031ec4f7ddb67f5186f7fad2b0a887
Michaeldq1 <michaeldquinton@gmail.com>
2023-02-20 07:16:46 -0600

    logic to prevent a user from marking a position that is already marked

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 const gameBoard = (() => {
   const board = document.getElementById("board");
-  const boardContents = ["x", "", "x", "x", "o", "x", "o", "o", "x"];
-  const fillBoard = () => {
-    boardContents.forEach((item) => {
-      const cell = document.createElement("div");
-      cell.classList.add("cell");
-      cell.textContent = item;
-      board.appendChild(cell);
-    });
+  const boardContents = ["", "", "x", "x", "o", "o", "o", "o", "x"];
+
+  const createCell = (content) => {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.textContent = content;
+    return cell;
   };
-  return { board, boardContents, fillBoard };
+
+  const fillBoard = () => {
+    const cells = boardContents.map(createCell);
+    board.append(...cells);
+  };
+
+  return { fillBoard };
 })();
 
 const playerFactory = (name, mark) => {
+  const isActive;
   const markPosition = () => {
-    const cells = document.getElementsByClassName("cell");
-    for (const cell of cells) {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {
       cell.addEventListener("click", () => {
-        cell.textContent = mark;
+        cell.textContent = cell.textContent === "" ? mark : cell.textContent;
       });
-    }
+    });
   };
-  return { name, mark, markPosition };
+  return { name, markPosition, isActive };
 };
-
-const displayController = () => {};
 
 gameBoard.fillBoard();
 
