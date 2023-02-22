4af782b08cfa513e914c88bb188072d1631d7068
Michaeldq1 <michaeldquinton@gmail.com>
2023-02-22 06:59:40 -0600

    startgame, markcell, switchplayer, checkwinner, resetboard functions in controller module

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const gameBoard = (() => {
   const board = document.getElementById("board");
-  const boardContents = ["", "", "x", "x", "o", "o", "o", "o", "x"];
+  const boardContents = ["", "", "", "", "", "", "", "", ""];
 
   const createCell = (content) => {
     const cell = document.createElement("div");
@@ -9,31 +9,107 @@ const gameBoard = (() => {
     return cell;
   };
 
+  const cells = boardContents.map(createCell);
+
   const fillBoard = () => {
-    const cells = boardContents.map(createCell);
     board.append(...cells);
   };
 
-  return { fillBoard };
+  return { cells, fillBoard };
 })();
 
 const playerFactory = (name, mark) => {
-  const isActive;
-  const markPosition = () => {
-    const cells = document.querySelectorAll(".cell");
-    cells.forEach((cell) => {
+  return { name, mark };
+};
+
+const controller = (() => {
+  const player1 = playerFactory("Tom", "X");
+  const player2 = playerFactory("Rob", "O");
+  let currentPlayer = player1;
+
+  const startGame = () => {
+    gameBoard.fillBoard();
+    controller.markCell();
+  };
+
+  const markCell = () => {
+    gameBoard.cells.forEach((cell) => {
       cell.addEventListener("click", () => {
-        cell.textContent = cell.textContent === "" ? mark : cell.textContent;
+        cell.textContent === ""
+          ? (cell.textContent = currentPlayer.mark)
+          : (cell.textContent = cell.textContent);
+        checkWinner(currentPlayer.mark);
+        switchPlayer();
       });
     });
   };
-  return { name, markPosition, isActive };
-};
 
-gameBoard.fillBoard();
+  const switchPlayer = () => {
+    if (currentPlayer === player1) {
+      currentPlayer = player2;
+    } else {
+      currentPlayer = player1;
+    }
+  };
 
-const player1 = playerFactory("Tom", "x");
-const player2 = playerFactory("Rob", "o");
+  const checkWinner = (mark) => {
+    const threeInARow = (cellOne, cellTwo, cellThree) => {
+      if (
+        gameBoard.cells[cellOne].textContent === mark &&
+        gameBoard.cells[cellTwo].textContent === mark &&
+        gameBoard.cells[cellThree].textContent === mark
+      ) {
+        console.log(currentPlayer.name + " is the winner");
+        return true;
+      } else {
+        return false;
+      }
+    };
 
-player1.markPosition();
-player2.markPosition();
+    const noWinner = (cells) => {
+      const checkTie = cells.some((cell) => cell.textContent === "");
+      checkTie === false ? console.log("Tie Game!") : console.log("No tie!");
+      return !checkTie;
+    };
+
+    switch (true) {
+      case threeInARow(0, 1, 2):
+        resetBoard();
+        break;
+      case threeInARow(3, 4, 5):
+        resetBoard();
+        break;
+      case threeInARow(6, 7, 8):
+        resetBoard();
+        break;
+      case threeInARow(0, 3, 6):
+        resetBoard();
+        break;
+      case threeInARow(1, 4, 7):
+        resetBoard();
+        break;
+      case threeInARow(2, 5, 8):
+        resetBoard();
+        break;
+      case threeInARow(0, 4, 8):
+        resetBoard();
+        break;
+      case threeInARow(2, 4, 6):
+        resetBoard();
+        break;
+      case noWinner(gameBoard.cells):
+        resetBoard();
+        break;
+    }
+  };
+
+  const resetBoard = () => {
+    gameBoard.cells.forEach((cell) => {
+      cell.textContent = "";
+    });
+  };
+
+  return { player1, player2, startGame, markCell };
+})();
+
+controller.startGame();
